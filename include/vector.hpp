8d4b1c5ac95f75551d7a4cf3153c554a11fe5d1b
Anish Kurani <anish.kurani09@gmail.com>
2026-03-19 22:53:58 -0400

    added basic vector operations

--- include/vector.hpp
+++ include/vector.hpp
@@ -17,6 +17,15 @@ namespace numc {
 
 		Vector(size_t capacity) : size(0), capacity(capacity), shape({ .rows = 1, .cols = 0}),
 			elements(std::make_unique<T[]>(capacity)) {}
+		
+		Vector(Vector&& other) noexcept
+			: size(other.size),
+			  capacity(other.capacity),
+			  shape(other.shape),
+			  elements(std::move(other.elements)) {
+			other.size = 0;
+			other.capacity = 0;
+		}
 
 		~Vector() = default;
 
@@ -37,9 +46,11 @@ namespace numc {
 		}
 
 		friend std::ostream& operator<<(std::ostream& os, const Vector<T>& vec) {
+			if (vec.size == 0) { os << "[ ]"; return os; }
+
 			os << "[ ";
 
-			for (std::size_t i = 0; i < vec.capacity; ++i) {
+			for (std::size_t i = 0; i < vec.size - 1; ++i) {
 				os << vec.elements[i] << ", ";
 			}
 
@@ -48,4 +59,38 @@ namespace numc {
 			return os;
 		}
 	};
+
+	template <typename T>
+	void scalarX(Vector<T>& vec, int x) {
+		for (size_t i = 0; i < vec.size; ++i) {
+			vec.elements[i] *= x;
+		}
+	}
+
+	template <typename T>
+	Vector<T> vecAdd(const Vector<T>& vec1, const Vector<T>& vec2) {
+		// add length check later
+		Vector<T> vec3(vec1.size);
+
+		for (size_t i = 0; i < vec1.size; ++i) {
+			vec3.elements[i] = vec1.elements[i] + vec2.elements[i];
+		}
+
+		vec3.size = vec1.size;
+		vec3.shape.cols = vec1.shape.cols;
+
+		return vec3;
+	}
+
+	template <typename T>
+	T dot(const Vector<T>& vec1, const Vector<T>& vec2) {
+		if (vec1.size != vec2.size) return -1; // invalid
+		
+		int dot = 0;
+
+		for (size_t i = 0; i < vec1.size; ++i) {
+			dot += vec1.elements[i] * vec2.elements[i];
+		}
+		return dot;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include "include/numc.hpp"
+#include "include/vector.hpp"
 #include <iostream>
 #include <memory>
 
 int main(void) {
 		
-	auto vec = numc::Vector<int>(9);
-
-	auto mat = numc::Matrix<int>(3, 3);
+	auto vec1 = numc::Vector<int>(3);
+	auto vec2 = numc::Vector<int>(3);
 	
-	std::cout << vec;
+	for (int i = 0; i < 3; ++i) {
+		vec1.push(i);
+		vec2.push(i + 1);
+	}
+	
+	std::cout << vec1;
+	
+	// auto vec3 = numc::vecAdd(vec1, vec2);
+	numc::scalarX(vec1, 3);	
+	// auto mat = numc::Matrix<int>(3, 3);
+	
+	std::cout << vec1;
+	// std::cout << vec2;
+	// std::cout << vec3;
+	// std::cout << vec3.shape.cols;
 
-	std::cout << mat;
-
+	// std::cout << mat;
+	
 	return 0;
 }
